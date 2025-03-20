d034b9f987bc63440f743628ef70a4d8b4d9c18e
Ryan McCormick <ryan.mccormick@rtmccormick.com>
2025-03-19 20:19:07 -0500

    updates

--- worker-bridge.js
+++ worker-bridge.js
@@ -1,70 +1,68 @@
-(function(global) {
-	'use strict';
+(function (global) {
+  'use strict';
 
-	/**
-	 * Adapter class that creates, manages and handles data transfer between
-	 * a script running on a page and a web worker script. 
-	 */
-	class WorkerBridge {
-		workerFn;
-		workerCtx;
+  /**
+   * Adapter class that creates, manages and handles data transfer between
+   * a script running on a page and a web worker script.
+   */
+  class WorkerBridge {
+    workerFn;
+    workerCtx;
 
-		constructor(workerFn) {
-			this.workerFn = workerFn;
-			this.workerCtx = this.initWorker();
-		}
+    constructor(workerFn) {
+      this.workerFn = workerFn;
+      this.workerCtx = this.initWorker();
+    }
 
-		async doWork(data) {
-			this.workerCtx.postMessage(data);
+    async doWork(data) {
+      this.workerCtx.postMessage(data);
 
-			return new Promise((resolve, reject) => {
-				let timeoutRef = -1;
+      return new Promise((resolve, reject) => {
+        let timeoutRef = -1;
 
-				function cleanup() {
-					if (timeoutRef > -1) {
-						clearTimeout(timeoutRef)
-					}
-				}
+        function cleanup() {
+          if (timeoutRef > -1) {
+            clearTimeout(timeoutRef);
+          }
+        }
 
-				function handleEvent(event) {
-					cleanup();
-					return resolve(event.data);
-				}
+        function handleEvent(event) {
+          cleanup();
+          return resolve(event.data);
+        }
 
-				function handleAsyncTimeout() {
-					cleanup();
-					return reject('[WorkerBridge]: async timeout has been exceeded.')
-				}
+        function handleAsyncTimeout() {
+          cleanup();
+          return reject('[WorkerBridge]: async timeout has been exceeded.');
+        }
 
-				this.workerCtx.addEventListener('message', handleEvent);
+        this.workerCtx.addEventListener('message', handleEvent);
 
-				timeoutRef = setTimeout(handleAsyncTimeout, 10000);
-			});
-		}
+        timeoutRef = setTimeout(handleAsyncTimeout, 10000);
+      });
+    }
 
-		initWorker() {
-			const workerBody = new Blob([`(${this.workerFn})(self)`], { type: 'application/javascript' });
-			return new Worker(URL.createObjectURL(workerBody))
-		}
+    initWorker() {
+      const workerBody = new Blob([`(${this.workerFn})(self)`], { type: 'application/javascript' });
+      return new Worker(URL.createObjectURL(workerBody));
+    }
 
-		dispose() {
-			if (this.workerCtx && this.workerCtx.onmessage) {
-				this.workerCtx.terminate();
-			}
-		}
-	}
+    dispose() {
+      if (this.workerCtx && this.workerCtx.onmessage) {
+        this.workerCtx.terminate();
+      }
+    }
+  }
 
-	// Add worker bridge to window/global context.
-	function init() {
-		const workerUtils = {
-			WorkerBridge: WorkerBridge
-		}
-		
-		global.WorkerBridge = global.WorkerBridge ?? WorkerBridge;
-	}
+  // Add worker bridge to window/global context.
+  function init() {
+    const workerUtils = {
+      WorkerBridge: WorkerBridge,
+    };
 
-	init();
-	/////////
+    global.WorkerBridge = global.WorkerBridge ?? WorkerBridge;
+  }
 
-	
-})(window)
+  init();
+  /////////
+})(window);
